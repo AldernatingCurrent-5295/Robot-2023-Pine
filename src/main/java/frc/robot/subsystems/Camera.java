2449684d75dee4f27dd0a343e0ca7e27636f8b34
MattSr92 <matthew_sr@comcast.net>
2023-01-14 14:39:45 -0800

    Camera Subsystem Update

--- src/main/java/frc/robot/subsystems/Camera.java
+++ src/main/java/frc/robot/subsystems/Camera.java
@@ -1,21 +1,42 @@
-/*
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj.IterativeRobotBase;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.CvSink;
+import edu.wpi.first.cscore.CvSource;
+import edu.wpi.first.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.TimedRobot;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
 
-public class Robot extends IterativeRobotBase {
-    CameraServer server;
+public class Robot extends TimedRobot {
+  Thread m_visionThread;
 
-    public void robotInit() {	
-        server = CameraServer.getInstance();
-        server.setQuality(50);
-        server.startAutomaticCapture("cam0");
-    }
+  @Override
+  public void robotInit() {
+    m_visionThread =
+        new Thread(
+            () -> {
+              UsbCamera camera = CameraServer.startAutomaticCapture();
+              camera.setResolution(640, 480);
+
+              CvSink cvSink = CameraServer.getVideo();
+              CvSource outputStream = CameraServer.putVideo("Rectangle", 640, 480);
+
+              Mat mat = new Mat();
+
+              while (!Thread.interrupted()) {
+                if (cvSink.grabFrame(mat) == 0) {
+                  outputStream.notifyError(cvSink.getError());
+                  continue;
+                }
+                Imgproc.rectangle(
+                    mat, new Point(100, 100), new Point(400, 400), new Scalar(255, 255, 255), 5);
+                outputStream.putFrame(mat);
+              }
+            });
+    m_visionThread.setDaemon(true);
+    m_visionThread.start();
+  }
 }
-*/

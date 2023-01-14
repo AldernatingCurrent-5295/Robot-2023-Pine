d7d37358b0427ea003dd64d89d21d74f9df9d266
MattSr92 <matthew_sr@comcast.net>
2023-01-14 14:11:57 -0800

    Camera Subsystem

---
+++ src/main/java/frc/robot/subsystems/Camera.java
@@ -0,0 +1,21 @@
+/*
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.IterativeRobotBase;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.cameraserver.CameraServer;
+
+public class Robot extends IterativeRobotBase {
+    CameraServer server;
+
+    public void robotInit() {	
+        server = CameraServer.getInstance();
+        server.setQuality(50);
+        server.startAutomaticCapture("cam0");
+    }
+}
+*/

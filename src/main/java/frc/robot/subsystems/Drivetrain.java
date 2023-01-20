797f9c00b56ddf1bc9a4a1be6b7afc906db55f00
AldernatingCurrent-5295 <aldernating5295current@gmail.com>
2023-01-20 15:04:40 -0800

    Add data to Dashboard

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -5,16 +5,17 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.DrivetrainConstants;
+import frc.robot.Constants.DrivetrainConfig;
 
 public class Drivetrain extends SubsystemBase {
 
-    private final CANSparkMax rfMotor = new CANSparkMax(DrivetrainConstants.rfMotorID, MotorType.kBrushless); 
-    private final CANSparkMax rbMotor = new CANSparkMax(DrivetrainConstants.rbMotorID, MotorType.kBrushless); 
+    private final CANSparkMax rfMotor = new CANSparkMax(DrivetrainConfig.rfMotorID, MotorType.kBrushless); 
+    private final CANSparkMax rbMotor = new CANSparkMax(DrivetrainConfig.rbMotorID, MotorType.kBrushless); 
 
-    private final CANSparkMax lfMotor = new CANSparkMax(DrivetrainConstants.lfMotorID, MotorType.kBrushless); 
-    private final CANSparkMax lbMotor = new CANSparkMax(DrivetrainConstants.lbMotorID, MotorType.kBrushless); 
+    private final CANSparkMax lfMotor = new CANSparkMax(DrivetrainConfig.lfMotorID, MotorType.kBrushless); 
+    private final CANSparkMax lbMotor = new CANSparkMax(DrivetrainConfig.lbMotorID, MotorType.kBrushless); 
 
     private final MotorControllerGroup m_rMotorControllerGroup = 
         new MotorControllerGroup(
@@ -40,6 +41,16 @@ public class Drivetrain extends SubsystemBase {
 
         m_lMotorControllerGroup.setInverted(false);
         m_rMotorControllerGroup.setInverted(true);
+    }
+
+    @Override
+    public void periodic() {
+        double[] motor_values = new double[4];
+        motor_values[0] = lfMotor.get();
+        motor_values[1] = rfMotor.get();
+        motor_values[2] = lbMotor.get();
+        motor_values[3] = rbMotor.get();
+        SmartDashboard.putNumberArray("RobotDrive Motors", motor_values);
     }
 
     public void tankDrive(Double rPwr, Double lPwr, boolean squareInputs) {

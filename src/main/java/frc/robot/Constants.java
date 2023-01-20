489b2cb7f19e6638278425c2568587afc862b1aa
AldernatingCurrent-5295 <aldernating5295current@gmail.com>
2023-01-20 15:06:32 -0800

    Added limelight values

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,10 +1,19 @@
 package frc.robot;
 
 public final class Constants {
-    public static class DrivetrainConstants {
+    public static class DrivetrainConfig {
         public static final int rfMotorID = 1;
         public static final int rbMotorID = 2;
         public static final int lfMotorID = 3;
         public static final int lbMotorID = 4;
+    }
+
+    /*
+     * All measurements in inches and degrees
+     */
+    public static class LimelightConfig {
+        public static final double llElevation = 12.9;
+        public static final double llAngle = 35.0;
+        public static final double goalElevation = 24.0;
     }
 }
